a87fe2dbd2391820c8a5efd94130ea5d861b0248
Omar Khan <o.khandxb@gmail.com>
2021-08-22 10:46:30 -0400

    test

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 const server = require('http').Server(app);
 const io = require('socket.io')(server);
 const { v4: uuidV4 } = require('uuid');
-var port = 3000 || process.env.PORT;
+var port = process.env.PORT || 3000;
 
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
